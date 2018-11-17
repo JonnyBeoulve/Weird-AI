4abd34a2b2329bdcfd700af782f49aa530e4c39d
Jonathan Leack <jleack@Jonathans-MacBook-Pro.local>
2018-11-17 11:51:21 -0800

    Built lyrics page with dilineation solution.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,14 +8,16 @@ import youtubeLogo from '../../static/img/youtube-logo.png';
 ======================================================================*/
 const Header = () => {
     return (
-        <header className="row">
-            <div className="header">
-                <div className="header-title">
-                    <h2>Weird AI</h2>
-                </div>
-                <div className="header-links">
-                    <a href="https://github.com/JonnyBeoulve/Weird-AI" target="_blank" rel="noopener noreferrer"><img src={githubLogo} className="header-link" alt="GitHub logo" /></a>
-                    <a href="https://www.youtube.com/channel/UCDBrVr0ttWpoRY-_yZajp2Q" target="_blank" rel="noopener noreferrer"><img src={youtubeLogo} className="header-link" alt="YouTube logo" /></a>
+        <header className="header">
+            <div className="row">
+                <div className="header-flex">
+                    <div className="header-flex-title">
+                        <h2>Weird AI</h2>
+                    </div>
+                    <div className="header-flex-links">
+                        <a href="https://github.com/JonnyBeoulve/Weird-AI" target="_blank" rel="noopener noreferrer"><img src={githubLogo} className="header-link" alt="GitHub logo" /></a>
+                        <a href="https://www.youtube.com/channel/UCDBrVr0ttWpoRY-_yZajp2Q" target="_blank" rel="noopener noreferrer"><img src={youtubeLogo} className="header-link" alt="YouTube logo" /></a>
+                    </div>
                 </div>
             </div>
         </header>
--- client/src/containers/HomeContainer.js/HomeContainer.js
+++ client/src/containers/HomeContainer.js/HomeContainer.js
@@ -5,12 +5,17 @@ import axios from 'axios';
 
 import '../../static/scss/main.scss';
 
+import lyricsFile from '../../store/lyrics.json';
+
 /* The container serves as the home for the app. */
 class HomeContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      lyrics: lyricsFile.lyrics,
       tagInput: '',
+      showInputCard: false,
+      showLyricsCard: true
     };
   }
 
@@ -54,15 +59,36 @@ class HomeContainer extends Component {
     return (
       <div className="home">
         <Header />
-        <div className="row">
-          <div className="home-card">
-            <h2 className="home-card-header">Throw us a word to write your own song.</h2>
-            <form onSubmit={this.handleTagSubmit}>
-              <input className="home-card-input" type="email" name="email" placeholder="Enter a tag" onChange={e => this.setState({ tagInput: e.target.value })} />
-              <button className="home-card-button" type="submit" value="Submit">Submit</button>
-            </form>
-          </div>
-        </div>
+        {(this.state.showInputCard)
+          ? <div className="row">
+              <div className="home-card">
+                <h2 className="home-card-header">Throw us a word to write your own song.</h2>
+                <form onSubmit={this.handleTagSubmit}>
+                  <input className="home-card-input" type="email" name="email" placeholder="Enter a tag" onChange={e => this.setState({ tagInput: e.target.value })} />
+                  <div className="home-card-buttons">
+                    <button className="home-card-button" type="submit" value="Submit">Generate a Song</button>
+                    <button className="home-card-button" type="submit" value="Submit">Can You Spot the Fake?</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          : null
+        }
+        {(this.state.showLyricsCard)
+          ? <div className="home-lyrics">
+              <h3 className="home-lyrics-text">
+              {this.state.lyrics.split("\\n").map((i,key) => {
+                return <div key={key}>{i}</div>;
+              })}
+              </h3>
+              <div className="home-lyrics-footer">
+                <div className="row">
+                  <button className="home-lyrics-footer-button" type="submit" value="Submit">Create New Song</button>
+                </div>
+              </div>
+            </div>
+          : null
+        }
       </div>
     );
   }
