0e33b08c8b8288d7fd13e313dd83bd5d9e26b94c
Jonathan Leack <jleack@Jonathans-MacBook-Pro.local>
2018-11-17 09:29:32 -0800

    Setup SCSS environment and folder structure.

---
+++ client/src/containers/HomeContainer.js/HomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../static/scss/main.scss';
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Weird AI</h2>
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
