d78ea6dccd4eea5559fec0e8a4dbc60c985a8186
Ivan Sued <thefenry@gmail.com>
2018-11-17 13:21:55 -0800

    Adding a Data Ingester system

---
+++ DataIngester/DataIngester/Models/MusicResponse.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DataIngester.Models
+{
+    public class MusicResponse
+    {
+        [JsonProperty("message")]
+        public Message Message { get; set; }
+    }
+
+    public class Message
+    {
+        [JsonProperty("body")]
+        public Body Body { get; set; }
+    }
+
+    public class Body
+    {
+        [JsonProperty("album_list")]
+        public List<Album> Albums { get; set; }
+
+
+        [JsonProperty("track_list")]
+        public List<Track> AlbumTracks { get; set; }
+
+        [JsonProperty("lyrics")]
+        public Lyrics Lyrics { get; set; }
+    }
+
+    public class Album
+    {
+        [JsonProperty("album")]
+        public AlbumDetail AlbumDetail { get; set; }
+    }
+
+    public class AlbumDetail
+    {
+        [JsonProperty("album_id")]
+        public int AlbumId { get; set; }
+
+        [JsonProperty("album_name")]
+        public string AlbumName { get; set; }
+    }
+
+    public class Track
+    {
+        [JsonProperty("track")]
+        public TrackDetail TrackDetail { get; set; }       
+    }
+
+    public class TrackDetail
+    {
+        [JsonProperty("track_id")]
+        public int TrackId { get; set; }
+
+        [JsonProperty("track_name")]
+        public string TrackName { get; set; }
+    }
+
+    public class Lyrics
+    {
+        [JsonProperty("lyrics_body")]
+        public string LyricsBody { get; set; }
+    }
+}
